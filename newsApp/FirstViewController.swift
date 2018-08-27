6c6c39e63980b616ee98e6ddd432e943309076f3
rohandangsharma <rohandangsharma@gmail.com>
2018-08-27 12:13:18 -0400

    Initial Commit

---
+++ newsApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  newsApp
+//
+//  Created by Rohan Sharma on 8/27/18.
+//  Copyright © 2018 Rohan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
